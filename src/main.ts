07a5f5512535635dc26f6f9bddce3e74787ef47c
Vsevolod Zhabskyi <Vsevolod_Zhabskyi@epam.com>
2022-08-26 01:41:46 +0300

    Initialize project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
+
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}
+
+bootstrap();
